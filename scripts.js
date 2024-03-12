2af0d8a830f49fca08b398b619d80cfb857ff5c2
joaovictordiass <joaovictorrochadias@gmail.com>
2024-03-12 09:03:22 -0300

    Prize Draw

--- scripts.js
+++ scripts.js
@@ -4,8 +4,12 @@ function generateNumber() {
     const inputMin = Math.ceil(document.querySelector(".input-min").value) // ARREDONDA O VALOR PARA BAIXO
     const inputMax = Math.floor(document.querySelector(".input-max").value) // ARREDONDA O VALOR PARA CIMA
     
-    const result = Math.floor(Math.random() * (inputMax - inputMin) + inputMin);
-    alert(result)
+    if(inputMin >= inputMax) {
+        alert("O valor mínimo tem que ser MENOR que o valor máximo!")
+    } else {
+        const result = Math.floor(Math.random() * (inputMax - inputMin) + inputMin);
+        alert(result)
+    }
 }
 
 
